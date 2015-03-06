5a0ac92dfe7336098f6d38c802ff92485056d33f
bernardagagnon <bernardg@gmail.com>
2015-03-06 10:53:43 -0500

    Update cachematrix.R
    Added function comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,10 @@
 ## Functions to invert and cache the result of square matrices
 
 ## Create an object that will wrap the matrix
+## args:     x: the square matrix to be inverted
+## returns:  a CacheMatrix object
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix(c(1.0,0.0,0.0,1.0),c(2,2))) {
     m <- NULL
     set <- function(y) {
         x <<- y
@@ -17,8 +19,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## function that inverts the matrix and caches the result for subsequent calls
-
+## function that inverts the matrix created by makeCacheMatrix and caches the result for subsequent calls
+## args:     x:   the CacheMatrix returned by makeCacheMatrix
+##           ...: additional arguments to be passed to solve
+## returns:  the inverted matrix if everything ok, NULL if the x argument is incorrect
 cacheSolve <- function(x, ...) {
   
   ## make sur we are using a proper object
